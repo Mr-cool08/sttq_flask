37d230a56f0bdba49ec1d19b5691b2b9e641e1b1
Liam <liam@suorsa.se>
2025-08-16 05:24:29 +0200

    port 80

--- app.py
+++ app.py
@@ -131,4 +131,4 @@ def transcribe_route():
         return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
